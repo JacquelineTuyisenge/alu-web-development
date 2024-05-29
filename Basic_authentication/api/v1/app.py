c1c3005ca314c3c17ec432a6e7976125e3f05399
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-29 20:52:58 +0200

    basic authentication

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -51,12 +51,14 @@ def before_request() -> None:
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
     ]
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if request.path not in excluded_paths:
+        if not auth.require_auth(request.path, excluded_paths):
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
