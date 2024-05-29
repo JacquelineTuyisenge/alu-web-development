e2d5bfbb4a17782ca777d08b7fc288395a1af997
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-29 20:30:49 +0200

    basic authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,12 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         '''Authorization header'''
-        return None
+
+        if request is None:
+            return None
+        if not "Authorization" in request.headers:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''Current user'''
