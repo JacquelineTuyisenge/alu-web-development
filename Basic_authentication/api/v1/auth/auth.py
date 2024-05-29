719d95f9c27c28868a741ee4450e909dcd1e7280
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-29 20:50:16 +0200

    basic authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -33,7 +33,7 @@ class Auth:
 
         if request is None:
             return None
-        if not "Authorization" in request.headers:
+        if "Authorization" not in request.headers:
             return None
         return request.headers.get("Authorization")
 
