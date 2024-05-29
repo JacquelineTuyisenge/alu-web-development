c7645a9f941b58df2f580d3da1c425f14fba73f4
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-29 02:41:08 +0200

    basic authentication

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -11,10 +11,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
