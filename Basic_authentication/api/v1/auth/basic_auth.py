e316eab90420b60f045be470e2b0b9fc00e13663
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-29 21:57:41 +0200

    basic authentication

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,15 +11,19 @@ import base64
 class BasicAuth(Auth):
     '''Basic Auth class that inherits from Auth'''
 
-    def extract_base64_authorization_header(self, au_h: str) -> str:
+    def extract_base64_authorization_header(
+    self, 
+    authorization_header: str
+) -> str:
+
         '''Extract base64 authorization header'''
 
-        if not au_h:
+        if not authorization_header:
             return None
 
-        if type(au_h) != str:
+        if type(authorization_header) != str:
             return None
 
-        if not au_h.startswith("Basic "):
+        if not authorization_header.startswith("Basic "):
             return None
-        return au_h.split(" ")[1:]
+        return authorization_header.split(" ")[1:]
