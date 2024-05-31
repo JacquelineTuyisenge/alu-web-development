66289c240e06b80d82efab8c5e2df76dead93c58
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-05-31 13:52:46 +0200

    session authenication

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -2,63 +2,71 @@
 """
 Route module for the API
 """
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from os import getenv
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
 auth = None
-
-if os.getenv("AUTH_TYPE") == "auth":
-    auth = Auth()
 if os.getenv("AUTH_TYPE") == "basic_auth":
     auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "session_auth":
+    auth = SessionAuth()
+elif os.getenv("AUTH_TYPE") == "session_exp_auth":
+    auth = SessionExpAuth()
+elif os.getenv("AUTH_TYPE") == "session_db_auth":
+    auth = SessionDBAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler """
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler """
-    print("401 Unauthorized error handler called")
+    """
+    Unauthorized handler.
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
-    """ Forbidden handler """
-    print("403 Forbidden error handler called")
+    """
+    Forbidden handler.
+    """
     return jsonify({"error": "Forbidden"}), 403
 
 
 @app.before_request
 def before():
-    """ Before request handler """
+    """
+    Before request.
+    """
     if auth:
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
-        ]
-        if not auth.require_auth(request.path, excluded_paths):
+        paths = ['/api/v1/status/',
+                 '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                 '/api/v1/auth_session/login/']
+        if not auth.require_auth(request.path, paths):
             return
-        if not auth.authorization_header(request):
+        if (not auth.authorization_header(request) and
+                not auth.session_cookie(request)):
             abort(401)
-        if not auth.current_user(request):
-            abort(403)
         request.current_user = auth.current_user(request)
         if not request.current_user:
             abort(403)
