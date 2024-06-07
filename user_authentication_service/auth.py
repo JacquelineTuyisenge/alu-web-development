3a2f58c4a74fa16bafc3b76f77508a801ccd1c70
Jacqueline Tuyisenge <j.tuyisenge@alustudent.com>
2024-06-07 21:16:20 +0200

    user authentication services

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -20,15 +20,16 @@ def _hash_password(password: str) -> str:
         return str(uuid4())
 
     class Auth:
-        '''auth class'''
+        """Auth class to interact with the authentication database.
+        """
 
         def __init__(self):
             self._db = DB()
 
         def register_user(self, email: str, password: str) -> User:
-            '''registering a user'''
+            """registering a user"""
             try:
-                already_exist_user = self._db.find_user_by(email=email)
+                existing_user = self._db.find_user_by(email=email)
                 raise ValueError(f'User {email} already exists')
             except NoResultFound:
                 hashed_password = _hash_password(password)
